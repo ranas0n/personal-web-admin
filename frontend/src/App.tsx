4104e2a8b0893450aae9210b2e24d9e7f4a47df2
BuildTools <nasutionrachmat@gmail.com>
2025-03-22 18:02:43 +0700

    display row number on the front page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,19 +5,22 @@ import DefaultLayout from './components/Layouts/DefaultLayout';
 import Loader from './components/common/Loader';
 import { RiStackOverflowFill } from 'react-icons/ri';
 import { getRowCount } from './services/dataOperations';
+import { AiFillProject } from 'react-icons/ai';
 
 function App() {
   // const [count, setCount] = useState(0)
   const [loading, setLoading] = useState<boolean>(true);
+  const [stackRowNum, setStackRowNum] = useState<number>(0);
+  const [projectRowNum, setProjectRowNum] = useState<number>(0);
 
   useEffect(() => {
     setTimeout(() => setLoading(false), 1000);
 
     const fetchRowCount = async () => {
-      const stackRowNum = await getRowCount('stacks');
-      const projectRowNum = await getRowCount('projects');
-      console.log(stackRowNum)
-      console.log(projectRowNum)
+      const stackRowRes = await getRowCount('stacks')
+      const projectRowRes = await getRowCount('projects')
+      setStackRowNum(stackRowRes.data);
+      setProjectRowNum(projectRowRes.data);
     }
 
     fetchRowCount();
@@ -28,8 +31,11 @@ function App() {
     <>
     <DefaultLayout>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
-        <CardDataStats title="Total views" total="$3.456K" rate="0.43%" levelUp>
+        <CardDataStats title="Total Number of Stacks" total={stackRowNum}>
           <RiStackOverflowFill />
+        </CardDataStats>
+        <CardDataStats title="Total Number of Projects" total={projectRowNum}>
+          <AiFillProject />
         </CardDataStats>
       </div>
     </DefaultLayout>
